ce7ebdcfc7f23849bb717b11f5604af07fc63ff8
Aleix Sanchis <aleixsanchis@hotmail.com>
2020-10-04 22:08:21 +0200

    Refactored main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import links
 from collatz import *
 from randomness import *
-def main():
-    print("This is the program from group 14!")
-    print("Please select one of the features below, stop by entering Q.")
-    options = """    1. Factorial
-    2. Randomness
-    3. Links
-    4. Collatz"""    
 
+options = """  1. Factorial
+  2. Randomness
+  3. Links
+  4. Collatz"""  
+
+def print_menu():
+  print("This is the program from group 14!")
+  print("Please select one of the features below, stop by entering Q.")
+  print(options)
+
+def main():
     opt=""
     while (opt!="Q"):
-        print(options)
+        print_menu()
         opt=input("Enter choice (stop with Q): ")
-        if (opt=="4"):
-            collatz_prompt()
+        if (opt == "1"):
+          pass
         elif(opt == "2"):
             randomness_prompt()
+        elif (opt == "3"):
+            links.links_prompt()
+        elif (opt=="4"):
+            collatz_prompt()
     
 
 if __name__ == "__main__":
